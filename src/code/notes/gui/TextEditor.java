0b108943923827302493209b6320f6086493f3fe
phwt <phwt.smwt@gmail.com>
2019-10-10 22:13:09 +0700

    Tabbed Editor

--- src/code/notes/gui/TextEditor.java
+++ src/code/notes/gui/TextEditor.java
@@ -7,9 +7,13 @@ package code.notes.gui;
 import code.notes.util.FileChooser;
 import code.notes.util.FileHandler;
 import code.notes.Bundle;
+import java.awt.*;
+import javax.swing.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,6 +33,7 @@ public class TextEditor extends javax.swing.JFrame {
      */
     public TextEditor() {
         initComponents();
+        addEmptyTab();
     }
 
     /**
@@ -40,31 +45,19 @@ public class TextEditor extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        scrollPane = new org.fife.ui.rtextarea.RTextScrollPane();
-        editorArea = new org.fife.ui.rsyntaxtextarea.RSyntaxTextArea();
-        //editorArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
-        //editorArea.setCodeFoldingEnabled(true);
+        jTabbedPane1 = new javax.swing.JTabbedPane();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         menuOpen = new javax.swing.JMenuItem();
         menuSave = new javax.swing.JMenuItem();
         menuSaveAs = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("code/notes/Bundle"); // NOI18N
         setTitle(bundle.getString("title")); // NOI18N
-
-        scrollPane.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setFoldIndicatorEnabled(true);
-        scrollPane.setLineNumbersEnabled(true);
-
-        editorArea.setColumns(20);
-        editorArea.setRows(60);
-        editorArea.setTabSize(4);
-        editorArea.setCodeFoldingEnabled(true);
-        editorArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
-        scrollPane.setViewportView(editorArea);
+        setMinimumSize(new java.awt.Dimension(500, 300));
 
         jMenu1.setText("File");
 
@@ -99,6 +92,15 @@ public class TextEditor extends javax.swing.JFrame {
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Edit");
+
+        jMenuItem1.setText("Add Tab");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem1);
+
         jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
@@ -107,11 +109,11 @@ public class TextEditor extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 803, Short.MAX_VALUE)
+            .addComponent(jTabbedPane1, javax.swing.GroupLayout.Alignment.TRAILING)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 509, Short.MAX_VALUE)
+            .addComponent(jTabbedPane1)
         );
 
         pack();
@@ -125,26 +127,27 @@ public class TextEditor extends javax.swing.JFrame {
         this.currentPath = currentPath;
         setTitle(Bundle.get("title") + " - " + currentPath);
     }
+    
+    public SingleEditor getCurrentEditor(){
+        return (SingleEditor) jTabbedPane1.getSelectedComponent();
+    }
 
     private void menuOpenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuOpenActionPerformed
         String path = FileChooser.open();
-        if (path != null) {
-            try {
-                editorArea.setText(FileHandler.open(path));
-            } catch (FileNotFoundException ex) {
-                Logger.getLogger(TextEditor.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            setCurrentPath(path);
-        }
+        addNewTab(path);
     }//GEN-LAST:event_menuOpenActionPerformed
 
     private void menuSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuSaveActionPerformed
-        if(getCurrentPath().isEmpty() || getCurrentPath() == null){
+        SingleEditor singleEditor = getCurrentEditor();
+        String currentText = singleEditor.getTextArea().getText();
+        
+        if(singleEditor.getPath().isEmpty()){
             menuSaveAsActionPerformed(evt);
             return;
         }
+        
         try {
-            FileHandler.save(editorArea.getText(), getCurrentPath());
+            FileHandler.save(currentText, singleEditor.getPath());
         } catch (IOException ex) {
             Logger.getLogger(TextEditor.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -152,23 +155,46 @@ public class TextEditor extends javax.swing.JFrame {
 
     private void menuSaveAsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuSaveAsActionPerformed
         String path = FileChooser.save();
+        SingleEditor singleEditor = getCurrentEditor();
+        String currentText = singleEditor.getTextArea().getText();
         try {
-            FileHandler.save(editorArea.getText(), path);
-            setCurrentPath(path);
+            FileHandler.save(currentText, path);
+            singleEditor.setContent(currentText);
+            
+            singleEditor.setPath(path);
+            jTabbedPane1.setTitleAt(singleEditor.getTabID(), singleEditor.getFileName());
         } catch (IOException ex) {
             Logger.getLogger(TextEditor.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_menuSaveAsActionPerformed
 
+    private void addNewTab(String path){
+        System.out.println(jTabbedPane1.getTabCount());
+        SingleEditor singleEditor = new SingleEditor(jTabbedPane1.getTabCount(), path);
+        jTabbedPane1.addTab(singleEditor.getFileName(), singleEditor);
+        jTabbedPane1.setSelectedIndex(singleEditor.getTabID());
+        System.out.println(singleEditor.getTabID());
+    }
+    
+    private void addEmptyTab(){
+        SingleEditor singleEditor = new SingleEditor(jTabbedPane1.getTabCount());
+        jTabbedPane1.addTab(singleEditor.getFileName(), singleEditor);
+        jTabbedPane1.setSelectedIndex(singleEditor.getTabID());
+    }
+    
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        addEmptyTab();
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private org.fife.ui.rsyntaxtextarea.RSyntaxTextArea editorArea;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JMenuItem menuOpen;
     private javax.swing.JMenuItem menuSave;
     private javax.swing.JMenuItem menuSaveAs;
-    private org.fife.ui.rtextarea.RTextScrollPane scrollPane;
     // End of variables declaration//GEN-END:variables
 
 }
