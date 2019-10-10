939ddd73436275b29aec69f63d702ce2fd8575e6
phwt <phwt.smwt@gmail.com>
2019-10-10 18:29:08 +0700

    Add menu bar

--- src/code/notes/gui/TextEditor.java
+++ src/code/notes/gui/TextEditor.java
@@ -20,10 +20,10 @@ import org.fife.ui.rsyntaxtextarea.*;
  *
  * @author phwts
  */
-public class TextEditor extends javax.swing.JFrame{
-        
+public class TextEditor extends javax.swing.JFrame {
+
     private String currentPath;
-    
+
     /**
      * Creates new form TextEditor
      */
@@ -40,40 +40,20 @@ public class TextEditor extends javax.swing.JFrame{
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
         scrollPane = new org.fife.ui.rtextarea.RTextScrollPane();
         editorArea = new org.fife.ui.rsyntaxtextarea.RSyntaxTextArea();
         //editorArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
         //editorArea.setCodeFoldingEnabled(true);
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        menuOpen = new javax.swing.JMenuItem();
+        menuSave = new javax.swing.JMenuItem();
+        menuSaveAs = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("code/notes/Bundle"); // NOI18N
         setTitle(bundle.getString("title")); // NOI18N
-
-        jButton1.setText(bundle.getString("open")); // NOI18N
-        jButton1.setToolTipText("");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                OpenActionPerformed(evt);
-            }
-        });
-
-        jButton2.setText(bundle.getString("save")); // NOI18N
-        jButton2.setToolTipText("");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                SaveActionPerformed(evt);
-            }
-        });
-
-        jButton3.setText(bundle.getString("save_as")); // NOI18N
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                SaveAsActionPerformed(evt);
-            }
-        });
 
         scrollPane.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setFoldIndicatorEnabled(true);
@@ -86,46 +66,67 @@ public class TextEditor extends javax.swing.JFrame{
         editorArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
         scrollPane.setViewportView(editorArea);
 
+        jMenu1.setText("File");
+
+        menuOpen.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_O, java.awt.event.InputEvent.CTRL_MASK));
+        menuOpen.setText(bundle.getString("open")); // NOI18N
+        menuOpen.setActionCommand(bundle.getString("open")); // NOI18N
+        menuOpen.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuOpenActionPerformed(evt);
+            }
+        });
+        jMenu1.add(menuOpen);
+
+        menuSave.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.CTRL_MASK));
+        menuSave.setText(bundle.getString("save")); // NOI18N
+        menuSave.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuSaveActionPerformed(evt);
+            }
+        });
+        jMenu1.add(menuSave);
+
+        menuSaveAs.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.SHIFT_MASK | java.awt.event.InputEvent.CTRL_MASK));
+        menuSaveAs.setText(bundle.getString("save_as")); // NOI18N
+        menuSaveAs.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuSaveAsActionPerformed(evt);
+            }
+        });
+        jMenu1.add(menuSaveAs);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton3)
-                .addContainerGap(591, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(12, 12, 12)
-                .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 803, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 482, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 509, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    public String getCurrentPath() {return currentPath;}
+    public String getCurrentPath() {
+        return currentPath;
+    }
+
     public void setCurrentPath(String currentPath) {
         this.currentPath = currentPath;
         setTitle(Bundle.get("title") + " - " + currentPath);
     }
-    
-    private void OpenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_OpenActionPerformed
+
+    private void menuOpenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuOpenActionPerformed
         String path = FileChooser.open();
         if (path != null) {
             try {
@@ -135,17 +136,21 @@ public class TextEditor extends javax.swing.JFrame{
             }
             setCurrentPath(path);
         }
-    }//GEN-LAST:event_OpenActionPerformed
+    }//GEN-LAST:event_menuOpenActionPerformed
 
-    private void SaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SaveActionPerformed
+    private void menuSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuSaveActionPerformed
+        if(getCurrentPath().isEmpty() || getCurrentPath() == null){
+            menuSaveAsActionPerformed(evt);
+            return;
+        }
         try {
             FileHandler.save(editorArea.getText(), getCurrentPath());
         } catch (IOException ex) {
             Logger.getLogger(TextEditor.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_SaveActionPerformed
+    }//GEN-LAST:event_menuSaveActionPerformed
 
-    private void SaveAsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SaveAsActionPerformed
+    private void menuSaveAsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuSaveAsActionPerformed
         String path = FileChooser.save();
         try {
             FileHandler.save(editorArea.getText(), path);
@@ -153,13 +158,16 @@ public class TextEditor extends javax.swing.JFrame{
         } catch (IOException ex) {
             Logger.getLogger(TextEditor.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_SaveAsActionPerformed
+    }//GEN-LAST:event_menuSaveAsActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private org.fife.ui.rsyntaxtextarea.RSyntaxTextArea editorArea;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem menuOpen;
+    private javax.swing.JMenuItem menuSave;
+    private javax.swing.JMenuItem menuSaveAs;
     private org.fife.ui.rtextarea.RTextScrollPane scrollPane;
     // End of variables declaration//GEN-END:variables
 
