0bb16cef459e4c4a1cbaf40abe7c800f37c4cae1
phwt <phwt.smwt@gmail.com>
2019-10-10 13:27:36 +0700

    Move file

---
+++ src/code/notes/main/CodeNotes.java
@@ -0,0 +1,15 @@
+/*
+ * Code-Notes | CodeNotes.java
+ * Created | 1:27:10 PM Oct 10, 2019
+ */
+package code.notes.main;
+
+/**
+ *
+ * @author phwts
+ */
+public class CodeNotes {
+    public static void main(String[] args) {
+        
+    }
+}
