5716bb416c04eff22125d9b80f103539af4c3a5d
phwt <phwt.smwt@gmail.com>
2019-10-12 18:53:45 +0700

    Create ExceptionLookup.java

---
+++ src/code/notes/gui/ExceptionLookup.java
@@ -0,0 +1,17 @@
+/*
+ * Code-Notes | ExceptionLookup.java
+ * Created | 6:52:43 PM Oct 12, 2019
+ */
+package code.notes.gui;
+
+import code.notes.util.ExceptionDatabase;
+
+/**
+ *
+ * @author phwts
+ */
+public class ExceptionLookup {
+    public static void main(String[] args) {
+        
+    }
+}
