2eb691e67cbc029b76261942e1711bbf6dbcef81
phwt <phwt.smwt@gmail.com>
2019-11-17 02:32:12 +0700

    Open file from Directory Listing

--- src/code/notes/gui/DirectoryListing.java
+++ src/code/notes/gui/DirectoryListing.java
@@ -5,20 +5,27 @@
 package code.notes.gui;
 
 import code.notes.util.UserPreferences;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
+import java.nio.file.Path;
+import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
 
 /**
  * @author Gilbert Le Blanc https://stackoverflow.com/a/23808238/
  */
 public class DirectoryListing extends javax.swing.JTree {
+
     static File fileRoot;
     static DefaultMutableTreeNode root;
     static DefaultTreeModel treeModel;
 
     public DirectoryListing() {
         this.init();
+        this.addListener();
     }
 
     public void init() {
@@ -29,9 +36,35 @@ public class DirectoryListing extends javax.swing.JTree {
         this.setModel(treeModel);
         this.setShowsRootHandles(true);
 
-        CreateChildNodes ccn
-                = new CreateChildNodes(fileRoot, root);
+        CreateChildNodes ccn = new CreateChildNodes(fileRoot, root);
         new Thread(ccn).start();
+    }
+
+    private Path toPath(TreePath path) {
+        FileNode user_path = (FileNode) ((DefaultMutableTreeNode) path.getLastPathComponent()).getUserObject();
+        File file = new File(user_path.toStringPath());
+        if (file.isDirectory())
+            return null;
+        return file.toPath();
+    }
+
+    public void addListener() {
+        JTree tree = this;
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int selected_row = tree.getRowForLocation(e.getX(), e.getY());
+                TreePath selected_path = tree.getPathForLocation(e.getX(), e.getY());
+                if (selected_row != -1) {
+                    if (e.getClickCount() == 2) {
+                        Path file_path = toPath(selected_path);
+                        if (file_path != null) {
+                            CodeNotes.text_editor.addTab(file_path);
+                        }
+                    }
+                }
+            }
+        });
     }
 
     public static class CreateChildNodes implements Runnable {
@@ -86,6 +119,10 @@ public class DirectoryListing extends javax.swing.JTree {
             } else {
                 return name;
             }
+        }
+
+        public String toStringPath() {
+            return file.getAbsolutePath();
         }
     }
 }
