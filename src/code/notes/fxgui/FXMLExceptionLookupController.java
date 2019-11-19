68c0716d45cb5795dd58e1214421afd9970f8b75
phwt <phwt.smwt@gmail.com>
2019-11-19 21:48:20 +0700

    Add exception lookup form

---
+++ src/code/notes/fxgui/FXMLExceptionLookupController.java
@@ -0,0 +1,26 @@
+/*
+ * Code-Notes-FX | FXMLExceptionLookupController.java
+ * Created | 9:22:12 PM Nov 19, 2019
+ */
+package code.notes.fxgui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author phwts
+ */
+public class FXMLExceptionLookupController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
