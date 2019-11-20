042177cb5d15546c9b513185cbc796849c1a4f21
phwt <phwt.smwt@gmail.com>
2019-11-20 19:52:17 +0700

    Directory Listing (With performance issues)

--- src/code/notes/fxgui/FXMLMainController.java
+++ src/code/notes/fxgui/FXMLMainController.java
@@ -5,9 +5,13 @@
  */
 package code.notes.fxgui;
 
+import code.notes.util.DirectoryNode;
 import code.notes.util.FileChooserDialog;
+import code.notes.util.UserPreferences;
+import java.io.File;
 import java.net.URL;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -18,6 +22,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.stage.Stage;
 
 /**
@@ -30,59 +36,89 @@ public class FXMLMainController implements Initializable {
 
     @FXML
     private TabPane tab_pane;
+    @FXML
+    private TreeView dir_tree;
 
     @FXML
     private void handleNewFileAction(ActionEvent event) {
         addTab();
     }
-    
+
     @FXML
     private void handleOpenAction(ActionEvent event) {
         // TODO: Make addTab handle multiple files by itself
         List<Path> paths = FileChooserDialog.openFiles(CodeNotes.STAGE);
-        if(paths != null)
-            for (Path path : paths)
+        if (paths != null) {
+            for (Path path : paths) {
                 this.addTab(path);
+            }
+        }
     }
-    
+
     @FXML
     private void handleSaveAction(ActionEvent event) {
         EditorTab selected_tab = (EditorTab) tab_pane.getSelectionModel().getSelectedItem();
         selected_tab.getEDITOR().save();
     }
-    
+
     @FXML
     private void handleSaveAsAction(ActionEvent event) {
         EditorTab selected_tab = (EditorTab) tab_pane.getSelectionModel().getSelectedItem();
         selected_tab.getEDITOR().saveAs();
     }
-    
+
     @FXML
-    private void handleExceptionLookupMenu(ActionEvent event) throws Exception{
+    private void handleExceptionLookupMenu(ActionEvent event) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLExceptionLookup.fxml"));
-        
+
         Scene scene = new Scene(root);
         Stage stage = new Stage();
-        
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-    }
-    
-    @FXML
-    private void handlePreferencesMenu(ActionEvent event) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLPreferences.fxml"));
-        
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();
-        
+
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
     }
 
+    @FXML
+    private void handlePreferencesMenu(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLPreferences.fxml"));
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    private void loadTree() {
+        
+        System.out.println(UserPreferences.getDirPath());
+        
+//        Path path = Paths.get("K:/Files/GitHub/Code-Notes-FX/nbproject/private");
+        Path path = Paths.get("K:/Files");
+//        Path path = Paths.get(UserPreferences.getDirPath());
+        DirectoryNode dir_node = new DirectoryNode(path);
+        dir_tree.setRoot(dir_node);
+        dir_tree.setShowRoot(false);
+        createTree(path, dir_node);
+    }
+
+    private void createTree(Path root_path, DirectoryNode parent) {
+        if (root_path.toFile().isDirectory()) {
+            DirectoryNode node = new DirectoryNode(root_path);
+            parent.getChildren().add(node);
+            for (File f : root_path.toFile().listFiles()) {
+                createTree(f.toPath(), node);
+            }
+        } else {
+            parent.getChildren().add(new DirectoryNode(root_path));
+        }
+    }
+
     /**
      * Add SingleEditor to editor_pool
+     *
      * @param tab
      */
     public static void addTabPool(EditorTab tab) {
@@ -91,12 +127,13 @@ public class FXMLMainController implements Initializable {
 
     /**
      * Remove SingleEditor from editor_pool
+     *
      * @param tab
      */
     public static void removeTabPool(EditorTab tab) {
         TAB_POOL.remove(tab);
     }
-    
+
     public static ArrayList<EditorTab> getTabPool() {
         return TAB_POOL;
     }
@@ -116,7 +153,7 @@ public class FXMLMainController implements Initializable {
                 return;
             }
         }
-        
+
         EditorTab editor_tab = new EditorTab(path);
         tab_pane.getTabs().add(editor_tab);
         tab_pane.getSelectionModel().select(editor_tab);
@@ -127,6 +164,7 @@ public class FXMLMainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         addTab();
+        loadTree();
     }
 
 }
